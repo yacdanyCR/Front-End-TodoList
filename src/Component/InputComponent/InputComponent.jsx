a6526a4c3f2ee8a5ca842ff6d1bc4d64c9073e63
Yacdany Arguedas Mora <jack20-21@hotmail.com>
2022-12-07 20:47:09 -0600

    Creating css

---
+++ src/Component/InputComponent/InputComponent.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './style.css'
+
+export const InputComponent = () => {
+    return (
+        <section>
+            <div className="task__Section">
+                <div className="container">
+                    <div className="task__Title">
+                        <h3>What is in your mind?</h3>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default InputComponent
---
+++ src/pages/IndexPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import HeaderComponent from '../Component/HeaderComponent/HeaderComponent'
+import InputComponent from '../Component/InputComponent/InputComponent'
+
+export const IndexPage = () => {
+    return (
+        <>
+            <HeaderComponent />
+            <main>
+                <InputComponent />
+            </main>
+        </>
+    )
+}
+
+export default IndexPage
