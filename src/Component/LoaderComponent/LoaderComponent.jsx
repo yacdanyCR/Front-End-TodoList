b0dc6f4ba448b0e3662d14dfe442c721e5403c8a
Yacdany Arguedas Mora <jack20-21@hotmail.com>
2022-12-09 09:50:48 -0600

    Creating loader component

---
+++ src/Component/LoaderComponent/LoaderComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './style.css'
+
+export const LoaderComponent = () => {
+    return (
+        /*    <div>
+               <h2 style={{ color: "black" }}>Loading...</h2>
+           </div> */
+        <div class="lds-dual-ring"></div>
+    )
+}
+
+export default LoaderComponent
--- src/Component/ShowingListTask/ShowingListTask.jsx
+++ src/Component/ShowingListTask/ShowingListTask.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { getTasks } from '../../services/TaskServices/TaskService'
 import ListTaskComponent from '../ListTaskComponent/ListTaskComponent'
+import LoaderComponent from '../LoaderComponent/LoaderComponent'
 import './style.css'
 
 export const ShowingListTask = () => {
@@ -13,11 +14,15 @@ export const ShowingListTask = () => {
     return (
         <section>
             <div className="showing__Task">
-                {Tasks.map((el, index) => {
-                    return (
-                        <ListTaskComponent task={el.task} id={el.id} key={index} completeded={el.completeded} />
-                    )
-                })}
+                {Tasks.length === 0
+                    ? <LoaderComponent />
+                    : Tasks.map((el, index) => {
+                        return (
+                            <ListTaskComponent task={el.task} id={el.id} key={index} completeded={el.completeded} />
+                        )
+                    })
+                }
+
             </div>
 
         </section>
