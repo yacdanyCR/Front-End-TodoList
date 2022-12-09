fb0e2372c3c29b2ce61b667256bea6637d8419cb
Yacdany Arguedas Mora <jack20-21@hotmail.com>
2022-12-08 19:35:37 -0600

    Adding a new style for tasks completed

--- src/Component/ListTaskComponent/ListTaskComponent.jsx
+++ src/Component/ListTaskComponent/ListTaskComponent.jsx
@@ -8,9 +8,9 @@ export const ListTaskComponent = (props) => {
     return (
         <div className='task_list' style={{ backgroundColor: `${randColor()}` }} key={props.index}>
             {props.completeded === 1
-                ? <><input type="checkbox" id="scales" name="scales" checked onClick={(e) => addCompleted(props.id, e.target.checked)}></input><label htmlFor="task">{props.task}</label></>
+                ? <><input type="checkbox" id="scales" name="scales" defaultChecked={true} onClick={(e) => addCompleted(props.id, e.target.checked)}></input><label htmlFor="task" className='task_Completed' >{props.task}</label></>
 
-                : <><input type="checkbox" id="scales" name="scales" onClick={(e) => addCompleted(props.id, e.target.checked)}></input><label htmlFor="task">{props.task}</label></>
+                : <><input type="checkbox" id="scales" name="scales" defaultChecked={false} onClick={(e) => addCompleted(props.id, e.target.checked)}></input><label htmlFor="task">{props.task}</label></>
             }
             <TiDelete size={30} color={"red"} onClick={() => deleteTask(props.id)} />
         </div>
