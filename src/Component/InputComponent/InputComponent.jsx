0546fbb26b4a2ef0684e38690ec788b6b9ba0eed
Yacdany Arguedas Mora <jack20-21@hotmail.com>
2022-12-14 08:32:57 -0600

    Showing alert when I added a task

--- src/Component/InputComponent/InputComponent.jsx
+++ src/Component/InputComponent/InputComponent.jsx
@@ -3,14 +3,18 @@ import './style.css'
 import { FcPlus } from "react-icons/fc";
 import { addTask, getTasks } from '../../services/TaskServices/TaskService';
 import { TaskContext } from '../../Context/TaskContext';
+import { successAlert } from '../../Alerts/alerts';
 
 export const InputComponent = () => {
     const [task, setNewTask] = useState("");
     const { setTasks } = useContext(TaskContext);
 
     const handleSubmit = async () => {
+        const $input = document.querySelector('#input');
         await addTask(task);
+        successAlert($input.value);
         await getTasks(setTasks);
+        $input.value = "";
     }
 
     return (
